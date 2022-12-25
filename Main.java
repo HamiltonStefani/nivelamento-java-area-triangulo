1a3cd02425f8a0909aad3db707ec76e177dc376d
HamiltonStefani <88253849+HamiltonStefani@users.noreply.github.com>
2022-12-25 20:12:26 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+package cursi_processamento;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double b, B, h, area;
+		b = 6.0;
+		B = 8.0;
+		h = 5.0;
+		area = (b + B)/2 * h;
+		
+		System.out.println(area);
+		
+		
+
+	}
+
+}
